86b4a5b6f859e641b199484b415b3cef05369fcf
siddhantprateek <siddhantprateek@gmail.com>
2022-06-29 17:27:35 +0530

    Thirdweb context provider added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,28 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import Layout from '../components/layout'
-import { Header } from '../components'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Layout from "../components/layout";
+import { Header } from "../components";
+import { ThirdwebProvider, ChainId } from '@thirdweb-dev/react'
+
+const desiredChainId = ChainId.Mumbai;
+
+// const supportedChainIds = [4];
+
+// const connectors = {
+//   injected: {},
+// };
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-  <Layout>
-    <Header />
-    <Component {...pageProps} />
-  </Layout>
-  )
+    <ThirdwebProvider
+      desiredChainId={desiredChainId}
+    >
+      <Layout>
+        <Header />
+        <Component {...pageProps} />
+      </Layout>
+    </ThirdwebProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
