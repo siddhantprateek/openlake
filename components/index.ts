479c3b6792d0b191706bfa7dfd9d8e858ed3895b
siddhantprateek <siddhantprateek@gmail.com>
2022-06-29 17:27:57 +0530

    default export for all components

---
+++ components/index.ts
@@ -0,0 +1,4 @@
+export { default as Header } from './header/header.components';
+export { default as Footer } from './footer/footer.components';
+export { default as Hero } from './hero/hero.components';
+export { default as Nftcard } from './nftcard/nftcard.components';
